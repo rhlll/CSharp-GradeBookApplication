6144b1b3cfd7dbb42492471a2df4e3d616419579
Rachel Lemay <rhlll@msn.com>
2019-09-27 10:28:19 -0400

    ps

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -27,6 +27,23 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
-
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked graidng requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
