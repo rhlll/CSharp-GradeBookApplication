03a6f65d747ac37aa6d9b185cbcb95ff7869e9a9
Rachel Lemay <rhlll@msn.com>
2019-09-27 12:07:44 -0400

    ps

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -128,10 +128,12 @@ namespace GradeBook.GradeBooks
                     gpa = 0;
                     break;
             }
-            if (IsWeighted && (studentType == StudentType.Honors) || (studentType == StudentType.DualEnrolled))
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
                 gpa++;
+            }
 
-                return gpa;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
